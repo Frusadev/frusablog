baa45180f79828ad7c4d56736c7b5efd558f208e
Frusadev <frusadev@gmail.com>
2025-06-18 08:56:06 +0000

    Fix - image optimization

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,21 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "ametsowou.me",
+        pathname: "/v1/resources/**",
+      },
+      {
+        protocol: "http",
+        hostname: "localhost",
+        port: "8000",
+        pathname: "/v1/resources/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
